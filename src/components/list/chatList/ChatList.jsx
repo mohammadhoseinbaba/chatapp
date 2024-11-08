a0fd422414686de2aa94cebad521c439ab91e4af
MRX <sirhb77@gmail.com>
2024-11-08 23:16:30 +0100

    adding chat list

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -3,7 +3,15 @@ import './chatlist.css'
 
 const ChatList = () => {
   return (
-    <div className='chatlist'>ChatList</div>
+    <div className='chatlist'>
+      <div className="search">
+        <div className="searchBar">
+            <img src="./search.png" alt="" />
+            <input type="text" placeholder='search'/>
+        </div>
+        <img src="./plus.png" alt="" />
+      </div>
+    </div>
   )
 }
 
