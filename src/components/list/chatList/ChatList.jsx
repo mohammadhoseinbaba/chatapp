10aadba93b4fa66b78e220137223ff59cec76081
MRX <sirhb77@gmail.com>
2024-11-09 00:55:11 +0100

    added feature of chatlist

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import './chatlist.css'
 
 
 const ChatList = () => {
+    const [addMode,setAddMode]=useState(false)
   return (
     <div className='chatlist'>
       <div className="search">
@@ -9,7 +11,35 @@ const ChatList = () => {
             <img src="./search.png" alt="" />
             <input type="text" placeholder='search'/>
         </div>
-        <img src="./plus.png" alt="" />
+        <img src={addMode ? "./minus.png":"./plus.png"} alt="" className='add' onClick={()=>{setAddMode(!addMode)}}/>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jan Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jan Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jan Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jan Doe</span>
+            <p>Hello</p>
+        </div>
       </div>
     </div>
   )
